225dc88c8f7482513210dd4736853ee1e921475f
Erik Estrada <erikestradaf21@gmail.com>
2021-09-01 00:02:06 -0300

    add crud

--- src/components/Local.jsx
+++ src/components/Local.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { store } from "../firebaseconfig";
 
 const Local = () => {
-    return (
-        <div>
-            <h2>Responsable del local</h2>
-        </div>
-    );
+  const [locales, setLocales] = useState([]);
+  useEffect(() => {
+    store.collection("locales").onSnapshot((querySnapshot) => {
+      let locales = [];
+      querySnapshot.docs.forEach((doc) => {
+        console.log(doc.data());
+        const { name, email } = doc.data();
+        locales.push({
+          id: doc.id,
+          name,
+          email,
+        });
+      });
+      setLocales(locales);
+    });
+    console.log(locales);
+  }, []);
+  return (
+    <div>
+      <h2>Responsable del local</h2>
+      {locales.map((local) => {
+        console.log(local);
+      })}
+    </div>
+  );
 };
 
 export default Local;
