589e58b8aa3a8d10a5547366edbd09136d3947ed
Erik Estrada <88987381+erikestradaf21@users.noreply.github.com>
2021-09-10 22:58:41 -0300

    update inicio.jsx

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { store } from '../firebaseconfig';
+import { auth } from '../firebaseconfig';
+import CRUD from '../crud'
+import db from '../firestore'
 
 const Inicio = () => {
+	const [usuario, setUsuario] = useState('')
+    useEffect( () => {
+        try {
+            auth.onAuthStateChanged( user => {
+                setUsuario(user.email);
+                //console.log(user.email);
+            })
+        } catch(err) {
+            console.log(err)
+        }
+        let get = CRUD
+        console.log('crud:')
+        console.log(get)
+        console.log('getAll:')
+        console.log(get.getAll())
+        
+    },[])
     return (
         <div>
             <h2>Inicio/Cliente</h2>
